46e364d17103c865996217519061b44ebd1ce69b
BraveCactus <mattdav961@gmail.com>
2025-11-22 00:08:10 +0300

    update anchor parameters

--- src/models/anchors.py
+++ src/models/anchors.py
@@ -1,29 +1,9 @@
 from torchvision.models.detection.rpn import AnchorGenerator
 
-PRESETS = {    
-    'small_vehicles': {
-        'sizes': ((8,), (16,), (32,), (64,), (128,)),  
-        'ratios': ((0.5, 1.0, 2.0),) * 5               
-    },    
-    
-    'medium_vehicles': {
-        'sizes': ((16,), (32,), (64,), (128,), (256,)),  
-        'ratios': ((0.5, 1.0, 2.0),) * 5                 
-    },    
-    
-    'large_vehicles': {
-        'sizes': ((32,), (64,), (128,), (256,), (512,)),  
-        'ratios': ((0.5, 1.0, 2.0),) * 5                  
-    },    
-    
-    'default': {
-        'sizes': ((32,), (64,), (128,), (256,), (512,)), 
-        'ratios': ((0.5, 1.0, 2.0),) * 5                
-    }
-}
+from src.config import ANCHOR_RATIOS, ANCHOR_SIZES
 
-def create_anchor_generator(sizes=((32,), (64,), (128,), (256,), (512,)), 
-                          ratios=((0.5, 1.0, 2.0),) * 5):                
+def create_anchor_generator(sizes=ANCHOR_SIZES, 
+                          ratios=ANCHOR_RATIOS):                
     """
     Создает anchor generator для FPN (5 feature maps)
     
@@ -41,17 +21,3 @@ def create_anchor_generator(sizes=((32,), (64,), (128,), (256,), (512,)),
         print(f"   Уровень {i+1}: размеры {size}, формы {ratio}")
     
     return anchor_generator
-
-def create_preset_anchor_generator(preset_name='medium_vehicles'):
-    """
-    Создает anchor generator из готового пресета
-    """
-    if preset_name not in PRESETS:
-        print(f"Пресет {preset_name} не найден, использую 'default'")
-        preset_name = 'default'
-    
-    preset = PRESETS[preset_name]
-    
-    print(f"Использую пресет: {preset_name}")
-    
-    return create_anchor_generator(preset['sizes'], preset['ratios'])
